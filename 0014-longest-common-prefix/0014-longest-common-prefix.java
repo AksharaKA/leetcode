70dcb051bf841b60723adc96d1bbf7daeccce747
Akshara K A <94751260+AksharaKA@users.noreply.github.com>
2023-08-06 00:28:36 +0530

    Time: 17 ms (5.47%), Space: 44.7 MB (5.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,13 +1,79 @@
 class Solution {
-     public String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0) return "";
-    String prefix = strs[0];
-    for (int i = 1; i < strs.length; i++)
-        while (strs[i].indexOf(prefix) != 0) {
-            prefix = prefix.substring(0, prefix.length() - 1);
-            if (prefix.isEmpty()) return "";
-        }        
-    return prefix;
-}
+    public String longestCommonPrefix(String[] strs) {
+        String prefix="";
+        int n;
+        n=strs.length;
+        int length=strs[0].length();
+        int length2;  
+        if(n>1)
+        {   int i=0,j=0;
+            int count=1;
+            length2=strs[1].length();
+            if(strs[0].length()>0&&strs[1].length()>0)
+            {      if(strs[0].charAt(0)==strs[1].charAt(0))
+                        {   prefix+=strs[0].charAt(0);
+                            i++;
+                            j++;
+                            while(count!=0 && j!=length2 && i!=length)
+                            {   if(strs[0].charAt(i)!=strs[1].charAt(j))
+                                {
+                                    count=0;
+                                }
+                                else
+                                {   prefix+=strs[0].charAt(i);
+                                    i++;
+                                    j++;
+                                }
+                            }
+                            
+                            int prelen;
+                            prelen=prefix.length();
+                            count=1;
+                            for(int m=2;m<n;m++)
+                            {   if(strs[m].length()==0||prefix.charAt(0)!=strs[m].charAt(0))
+                                {	count=0;
+                                    prefix="";
+                                    break;    
+                                }                  
+                            }
+                            int p=1,q=1;
+                            if(count!=0)
+                            {   for(int m=2;m<n;m++)
+                                { 
+                                count=1; 
+                                p=1;q=1;
+                                String prefix1="";
+                                prefix1+=prefix.charAt(0);
+                                while(count!=0 && q!=strs[m].length() && p!=prefix.length())
+                                {   if(strs[m].charAt(q)!=prefix.charAt(p))
+                                    {
+                                        count=0;
+                                    }
+                                    else
+                                    {   
+                                        prefix1+=strs[m].charAt(p);
+                                        p++;
+                                        q++;
+                                    }
+                                }
+                                prefix=prefix1;
+                                System.out.println(prefix); 
+                                System.out.println(count); 
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            return prefix;
+                        }  
+            }
+        }
+        else
+        {
+            prefix=strs[0];
+        }
+        return prefix;
+    }
 }
 
