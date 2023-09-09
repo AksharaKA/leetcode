a5d5ddd64d6400fd0e9e9571c9fb5becdb97bee0
Akshara K A <94751260+AksharaKA@users.noreply.github.com>
2023-09-09 14:12:44 +0530

    Time: 359 ms (6.99%), Space: 44.4 MB (68.6%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int len=nums.length;
+        for(int i=0;i<len;i++)
+        {   int count=0;
+            int a=nums[i];
+            for(int j=0;j<len;j++)
+            {
+                if(a==nums[j])
+                {
+                    count++;
+                }
+            }
+            if(count<2)
+                {
+                    return a;
+                }
+        }
+        return 0;
+    }
+}
